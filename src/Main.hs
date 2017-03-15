c43b5a184dd7a22d953e91065130339711d753a9
Ignat Insarov <kindaro@gmail.com>
2017-03-15 06:26:57 +0300

    Add some structure to the code.

--- src/Main.hs
+++ src/Main.hs
@@ -22,13 +22,10 @@ main :: IO ()
 main = do
     args <- getArgs
     let target = args !! 0
-    let limit = read (args !! 1)
     id <- parseId <$> api "users.get" [ ("user_ids", target) ]
-    friends <- parseFriends limit <$> api "friends.get" [ ("user_id", T.unpack id) ]
-    print friends
-    friendsNames <- resolveToNames friends
-    print friendsNames
-    friendsSquared <- Prelude.sequence $ fmap (parseFriends limit) . api "friends.get" <$> ( idToApiArg <$> friends )
+    friends <- getFriends id
+    friendsNames <- getNames friends
+    friendsSquared <- Prelude.sequence $ fmap parseFriends . api "friends.get" <$> ( idToArg <$> friends )
     print $ friendsNames `zip` friendsSquared
 
     -- Compute mean.
@@ -36,31 +33,44 @@ main = do
     -- Get walls.
     -- Find correlation between wall length and friends length.
 
-parseId :: Data.Aeson.Value -> T.Text
-parseId = T.pack . show . fromMaybe (error "No parse for ID!") . (^? nth 0 . key "uid" . _Integral )
+-- ** Parser functions.
+-- All of these have a general signature of form:
+-- f :: Data.Aeson.Value -> a
+-- -- or just for short:
+-- f :: Value -> a
 
-parseName :: Data.Aeson.Value -> T.Text
+parseId :: Value -> Int -- ^ Operates on "users.get".
+parseId = fromMaybe (error "No parse for ID!") . (^? nth 0 . key "uid" . _Integral )
+
+parseName :: Value -> T.Text -- ^ Operates on "users.get".
 parseName = fromMaybe (error "No parse for name!") . (^? nth 0 . key "first_name" . _String )
 
-parseFriends :: Int -> Data.Aeson.Value -> [Int]
-parseFriends limit = take limit . ( ^.. values . _Integral )
+parseNames :: Value -> [ (T.Text, T.Text) ] -- ^ Operates on "users.get".
+parseNames
+        = ( (^.. values . key "first_name" . _String)
+        &&& (^.. values . key "last_name"  . _String)
+          ) >>> uncurry zip 
 
-idToApiArg :: Int -> [ (String, String) ]
-idToApiArg = pure . ("user_id", ) . show
+parseFriends :: Value -> [Int] -- ^ Operates on "friends.get".
+parseFriends = ( ^.. values . _Integral )
 
-resolveToNames :: [Int] -> IO [ (T.Text, T.Text) ]
-resolveToNames
-     =   fmap show
-     >>> intercalate ","
-     >>> ("user_ids", )
-     >>> pure
-     >>> api "users.get"
-     >>> fmap   (
-                    (   (^.. values . key "first_name" . _String)
-                    &&& (^.. values . key "last_name"  . _String)
-                    )
-                >>> uncurry zip
-                )
+-- ** Various pure helper functions used to construct arguments to api calls.
+
+idToArg :: Int -> [ (String, String) ]
+idToArg = pure . ("user_id", ) . show
+
+idsToArg :: [Int] -> [ (String, String) ]
+idsToArg xs = [ ("user_ids", intercalate "," . fmap show $ xs) ]
+
+-- ** Getter functions.
+-- All of these have a general signature of form:
+-- f :: a -> IO b
+
+getNames :: [Int] -> IO [ (T.Text, T.Text) ]
+getNames =  idsToArg >>> api "users.get" >>> fmap parseNames
+
+getFriends :: Int -> IO [Int]
+getFriends u = parseFriends <$> api "friends.get" [ ("user_id", show u) ]
 
 api :: FilePath -> [ (String, String) ] -> IO Value
 api method args = parse <$> get url
@@ -81,4 +91,11 @@ api method args = parse <$> get url
         ( error . show $ r ^. responseBody . key "error" . key "error_msg" . _String )
         ( r ^? responseBody . key "response" )
 
+-- I need a function that does [uid] -> [uid, [uid] (friends)] conversion.
+-- Then, I need another that does [uid, [uid] (friends)] -> [uid, [uid, [uid] (friends ^ 2) ] ].
+-- Then, I need a third one that goes [uid, [uid, [uid]]] -> [uid, [uid]].
+-- It would be a return, an fmap and a join.
 
+-- nReturn :: [Int] -> (IO) [(Int, [Int])]
+-- nReturn us = parseFriends 10 <$> api "friends.get"
+-- nFmap :: [(Int, [Int])] -> [(Int, (IO) [(Int, [Int])])]
